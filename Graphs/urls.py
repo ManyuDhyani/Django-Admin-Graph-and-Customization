7e16236775e5b46074100adf008df328f9719abe
ManyuDhyani <manyudhyani@gmail.com>
2022-02-27 11:27:50 +0530

    Implemented: Bar Graph With sum population of each country

--- Graphs/urls.py
+++ Graphs/urls.py
@@ -1,8 +1,9 @@
 from django.urls import path
-from .views import pie_chart
+from .views import pie_chart, population_chart
 
 app_name = 'Graphs'
 
 urlpatterns = [
     path('pie-chart/', pie_chart, name='pie-chart'),
+    path('population-chart/', population_chart, name='population-chart'),
 ]
--- Graphs/views.py
+++ Graphs/views.py
@@ -1,17 +1,33 @@
 from django.shortcuts import render
 from .models import City
+from django.db.models import Sum
+from django.http import JsonResponse
 
 def pie_chart(request):
     labels = []
     data = []
 
-    queryset = City.objects.order_by('-population')
-    print(queryset)
+    queryset = City.objects.order_by('population')
     for city in queryset:
         labels.append(city.name)
         data.append(city.population)
 
     return render(request, 'pieChart.html', {
+        'labels': labels,
+        'data': data,
+    })
+
+
+def population_chart(request):
+    labels = []
+    data = []
+
+    queryset = City.objects.values('country__name').annotate(country_population=Sum('population')).order_by('-country_population')
+    for entry in queryset:
+        labels.append(entry['country__name'])
+        data.append(entry['country_population'])
+    
+    return render(request, 'populationChart.html', {
         'labels': labels,
         'data': data,
     })
